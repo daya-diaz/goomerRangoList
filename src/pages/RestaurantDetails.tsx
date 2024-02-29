e4708ec20466d080bdf5376631333183362e8d8d
Dayane Santos <dayadiazprofissional@gmail.com>
2024-02-29 13:24:38 -0300

    feat: Criando lógica para se o restaurante não possuir um determinado tipo de comida, o details desse tipo não renderizar.

--- src/pages/RestaurantDetails.tsx
+++ src/pages/RestaurantDetails.tsx
@@ -3,7 +3,7 @@ import { RESTAURANTS } from '../utils/data';
 import img from '../assets/restaurantImg.png';
 import SearchIcon from '../assets/search.svg'
 import { Header } from '../components/Header';
-import arrow from  '../assets/arrowDown.png'
+import arrow from '../assets/arrowDown.png'
 import FoodCard from '../components/FoodCard';
 
 export default function RestaurantDetails() {
@@ -50,24 +50,67 @@ export default function RestaurantDetails() {
               />
             </form>
           </div>
-          
-          <details>
-            <summary className='flex items-center justify-between w-[300px] md:w-[50%] lg:w-[75%] p-3 border-b-2 border-colorText'>
-              <span className='text-colorText font-semibold'>Almoços</span>
-              <img className='w-4 h-[9px]' src={arrow} />
-            </summary>
-            <ul className='flex flex-wrap pt-6 justify-between gap-5 w-[300px] md:w-[50%] lg:w-[75%]'>
-              {
-                restaurant?.foodsList.map(food => {
-                  if(food.type === 'lunch') {
-                    return <FoodCard key={food.id} id={food.id} name={food.name} description={food.description} value={food.value} pic={food.foodImg}/>
-                  } else {
-                    return
+          {/*Lógica para se o restaurante não tiver algum determinado tipo de comida, o details não renderizar*/}
+          {restaurant?.foodsList.some(food => food.type === 'lunch') && (
+            <details>
+              <summary className='flex items-center justify-between w-[300px] md:w-[50%] lg:w-[75%] p-3 border-b-2 border-colorText'>
+                <span className='text-colorText font-semibold'>Almoços</span>
+                <img className='w-4 h-[9px]' src={arrow} />
+              </summary>
+              <ul className='flex flex-wrap pt-6 justify-between gap-5 w-[300px] md:w-[50%] lg:w-[75%]'>
+                {
+                  restaurant?.foodsList.map(food => {
+                    if (food.type === 'lunch') {
+                      return <FoodCard key={food.id} id={food.id} name={food.name} description={food.description} value={food.value} pic={food.foodImg} />
+                    } else {
+                      return
+                    }
+                  })
+                }
+              </ul>
+            </details>
+          )}
+
+          {restaurant?.foodsList.some(food => food.type === 'drink') && (
+              <details>
+                <summary className='flex items-center justify-between w-[300px] md:w-[50%] lg:w-[75%] p-3 border-b-2 border-colorText'>
+                  <span className='text-colorText font-semibold'>Bebidas</span>
+                  <img className='w-4 h-[9px]' src={arrow} />
+                </summary>
+                <ul className='flex flex-wrap pt-6 justify-between gap-5 w-[300px] md:w-[50%] lg:w-[75%]'>
+                  {
+                    restaurant?.foodsList.map(food => {
+                      if (food.type === 'drink') {
+                        return <FoodCard key={food.id} id={food.id} name={food.name} description={food.description} value={food.value} pic={food.foodImg} />
+                      } else {
+                        return
+                      }
+                    })
                   }
-                })
-              }         
-            </ul>
-          </details>
+                </ul>
+              </details>
+          )}
+
+          {restaurant?.foodsList.some(food => food.type === 'dessert') && (
+            <details>
+              <summary className='flex items-center justify-between w-[300px] md:w-[50%] lg:w-[75%] p-3 border-b-2 border-colorText'>
+                <span className='text-colorText font-semibold'>Sobremesas</span>
+                <img className='w-4 h-[9px]' src={arrow} />
+              </summary>
+              <ul className='flex flex-wrap pt-6 justify-between gap-5 w-[300px] md:w-[50%] lg:w-[75%]'>
+                {
+                  restaurant?.foodsList.map(food => {
+                    if (food.type === 'dessert') {
+                      return <FoodCard key={food.id} id={food.id} name={food.name} description={food.description} value={food.value} pic={food.foodImg} />
+                    } else {
+                      return null;
+                    }
+                  })
+                }
+              </ul>
+            </details>
+          )}
+
         </div>
         <div className='w-[25%] bg-bg'></div>
       </div>
